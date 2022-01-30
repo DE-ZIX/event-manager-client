d2f703506cbd95150abd52c95f767512ea1986a2
Giovani Henriques <giovanifvhenriques@gmail.com>
2022-01-30 11:40:52 -0300

    adding axios and vue-axios

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,12 @@ import router from './router';
 import store from './store';
 import { Quasar } from 'quasar';
 import quasarUserOptions from './quasar-user-options';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 
 createApp(App)
 	.use(Quasar, quasarUserOptions)
 	.use(store)
+	.use(VueAxios, axios)
 	.use(router)
 	.mount('#app');
