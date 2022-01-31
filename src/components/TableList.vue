7b57ef3434d7a92d08b9be4808c253304345d947
Giovani Henriques <giovanifvhenriques@gmail.com>
2022-01-31 00:17:37 -0300

    views init

---
+++ src/components/TableList.vue
@@ -0,0 +1,29 @@
+<template>
+	<div>
+		<q-table></q-table>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import { ConsultList } from '@/models';
+
+export default defineComponent({
+	props: {
+		items: {
+			type: ConsultList,
+		},
+	},
+	setup() {
+		return {
+			pageInformation: ref({
+				page: 1,
+				descending: true,
+				sortBy: 'id',
+				rowsPerPage: 5,
+				rowsNumber: 0,
+			}),
+		};
+	},
+});
+</script>
---
+++ src/views/home/Home.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="home">
+		<!-- <img alt="Vue logo" src="@assets/logo.png" />
+		<HelloWorld msg="Welcome to Your Vue.js + TypeScript App" /> -->
+		<resource-list />
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import ResourceList from '../home/ResourceList.vue';
+
+export default defineComponent({
+	name: 'Home',
+	components: {
+		ResourceList,
+	},
+});
+</script>
