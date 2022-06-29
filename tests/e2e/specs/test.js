7cb43e3097e09e207f3ab6bfb02f6a264d9b1086
Giovani Henriques <giovanifvhenriques@gmail.com>
2022-06-29 16:23:22 -0300

    test/home default test

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,16 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe('My First Test', () => {
+describe('Home basic test', () => {
 	it('Visits the app root url', () => {
 		cy.visit('/');
-		cy.contains('h1', 'Welcome to Your Vue.js + TypeScript App');
+		cy.get('#resource_list').should('exist');
+		cy.get('#event_list').should('exist');
+		cy.get('#class_list').should('exist');
+		// cy.get('body').then(($body) => {
+		// 	const containsResourceList = $body.find('#resource-list').length > 0;
+		// 	const cotainsEventList = $body.find('#event-list').length > 0;
+		// 	const containsClassList = $body.find('#class-list').length > 0;
+		// 	return false;
+		// });
 	});
 });
