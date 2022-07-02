c69739ae10a4e1defae2c0abcc03db379d99a824
Giovani Henriques <giovanifvhenriques@gmail.com>
2022-07-02 17:26:40 -0300

    CI/breaking test on purpose

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -4,6 +4,6 @@ import Home from '@/views/home/Home.vue';
 describe('Home.vue', () => {
 	it('renders lists properly', () => {
 		const wrapper = shallowMount(Home);
-		expect(wrapper.find('.home').exists()).toBe(true);
+		expect(wrapper.find('.home').exists()).toBe(false);
 	});
 });
